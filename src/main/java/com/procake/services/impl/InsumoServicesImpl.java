ef96caab573d60091014727f28f2cf707372771f
Maria Lídia <88863501+marialidiaa@users.noreply.github.com>
2023-05-18 21:26:26 -0300

    ajuste de lancamento de insumo

--- src/main/java/com/procake/services/impl/InsumoServicesImpl.java
+++ src/main/java/com/procake/services/impl/InsumoServicesImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -13,22 +15,28 @@ import com.procake.exceptions.RecursoNaoEncontradoException;
 import com.procake.mappers.SimpleMapper;
 import com.procake.repositories.EstoqueRespositories;
 import com.procake.repositories.InsumoRepositories;
+import com.procake.services.IEstoqueServices;
 import com.procake.services.IInsumoServices;
 import com.procake.utils.ToUpper;
+import com.procake.v1.controllers.EstoqueController;
 import com.procake.v1.dtos.InsumoDTO;
 import com.procake.v1.models.EstoqueModel;
 import com.procake.v1.models.InsumoModel;
 
 @Component
 public class InsumoServicesImpl implements IInsumoServices{
-	
+
 	private InsumoRepositories repository;
 	private EstoqueRespositories estoqueRepository;
-	
-	public InsumoServicesImpl(InsumoRepositories repository, EstoqueRespositories estoqueRepository) {
+	private IEstoqueServices estoqueServices;
+
+	public InsumoServicesImpl(InsumoRepositories repository, EstoqueRespositories estoqueRepository, IEstoqueServices estoqueServices) {
 		this.repository = repository;
 		this.estoqueRepository = estoqueRepository;
+		this.estoqueServices = estoqueServices;
 	}
+
+	Logger logger = LoggerFactory.getLogger(InsumoServicesImpl.class);
 
 	@Override
 	public InsumoDTO buscarPorId(UUID id) {
@@ -39,39 +47,77 @@ public class InsumoServicesImpl implements IInsumoServices{
 	@Override
 	public Page<InsumoDTO> listarTodos(Pageable pageable) {
 		Page<InsumoModel> page = repository.findAll(pageable);
-		return page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+		Page<InsumoDTO> pageDTO = page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+		for(int i = 0; i < pageDTO.getTotalElements(); i ++) {
+			List<EstoqueModel> listEstoqueModel = estoqueServices.listarPorIdInsumo(pageDTO.toList().get(i).getId());
+			Double quantidade = 0.0;
+			for(int j = 0; j < listEstoqueModel.size(); j ++) {
+				quantidade += listEstoqueModel.get(j).getQuantidade();
+			}
+			pageDTO.toList().get(i).setQuantidade(quantidade);
+		}
+		return pageDTO;
 	}
-	
+
 	@Override
 	public Page<InsumoDTO> listarTodosAtivos(Pageable pageable) {
 		Page<InsumoModel> page = repository.listarTodosAtivos(pageable);
-		return page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+
+		Page<InsumoDTO> pageDTO = page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+		for(int i = 0; i < pageDTO.getTotalElements(); i ++) {
+			List<EstoqueModel> listEstoqueModel = estoqueServices.listarPorIdInsumo(pageDTO.toList().get(i).getId());
+			Double quantidade = 0.0;
+			for(int j = 0; j < listEstoqueModel.size(); j ++) {
+				quantidade += listEstoqueModel.get(j).getQuantidade();
+			}
+			pageDTO.toList().get(i).setQuantidade(quantidade);
+		}
+		return pageDTO;
 	}
-	
+
 	@Override
 	public Page<InsumoDTO> listarTodosDesativados(Pageable pageable) {
 		Page<InsumoModel> page = repository.listarTodosDesativados(pageable);
-		return page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+		Page<InsumoDTO> pageDTO = page.map(i -> SimpleMapper.INSTANCE.insumo2InsumoDTO(i));
+		for(int i = 0; i < pageDTO.getTotalElements(); i ++) {
+			List<EstoqueModel> listEstoqueModel = estoqueServices.listarPorIdInsumo(pageDTO.toList().get(i).getId());
+			Double quantidade = 0.0;
+			for(int j = 0; j < listEstoqueModel.size(); j ++) {
+				quantidade += listEstoqueModel.get(j).getQuantidade();
+			}
+			pageDTO.toList().get(i).setQuantidade(quantidade);
+		}
+		return pageDTO;
 	}
 
 	@Override
 	public List<InsumoDTO> buscarPorNome(String nome) {
-		
+
 		if(nome.equals("undefined")) {
 			nome = " ";
 		}
 
-		List<InsumoModel> listUser = repository.buscarPorNome(nome.trim().toUpperCase());
+		List<InsumoModel> listInsumo = repository.buscarPorNome(nome.trim().toUpperCase());
 		List<InsumoDTO> list = new ArrayList<>();	
-		 listUser.forEach(item -> list.add(SimpleMapper.INSTANCE.insumo2InsumoDTO(item)));
+		listInsumo.forEach(item -> list.add(SimpleMapper.INSTANCE.insumo2InsumoDTO(item)));
+		
+		for(int i = 0; i < list.size(); i ++) {
+			List<EstoqueModel> listEstoqueModel = estoqueServices.listarPorIdInsumo(list.get(i).getId());
+			Double quantidade = 0.0;
+			for(int j = 0; j < listEstoqueModel.size(); j ++) {
+				quantidade += listEstoqueModel.get(j).getQuantidade();
+			}
+			list.get(i).setQuantidade(quantidade);
+		}
+	
 		return list;
 	}
 
 	@Override
 	public InsumoDTO inserir(InsumoDTO insumoDTO) {
-		
+
 		InsumoModel model = SimpleMapper.INSTANCE.insumoDTO2Insumo(insumoDTO);
-		
+
 		ToUpper.UPPER_INSUMO_MODEL(model);
 		model = repository.saveAndFlush(model);
 
@@ -80,14 +126,14 @@ public class InsumoServicesImpl implements IInsumoServices{
 
 	@Override
 	public InsumoDTO atualizar(UUID id, InsumoDTO insumo) {
-	
+
 		InsumoModel model = repository.findById(id)
 				.orElseThrow(() -> new RecursoNaoEncontradoException("Insumo não encontrado com esse id: " + id));
 
 		//Atualização de status do insumo
 		if(insumo.isEnabled() != model.isEnabled()) {
 			List<EstoqueModel> estoque = estoqueRepository.buscarPorInsumoAtivo(model.getId());
-			
+
 			if(estoque.size() > 0) {
 				throw new OperacaoInvalidaException("Para desativar o insumo é preciso que o estoque dele esteja zerado");
 			}
@@ -110,7 +156,7 @@ public class InsumoServicesImpl implements IInsumoServices{
 
 		List<InsumoModel> listUser = repository.buscarAtivoPorNome(nome.trim().toUpperCase());
 		List<InsumoDTO> list = new ArrayList<>();	
-		 listUser.forEach(item -> list.add(SimpleMapper.INSTANCE.insumo2InsumoDTO(item)));
+		listUser.forEach(item -> list.add(SimpleMapper.INSTANCE.insumo2InsumoDTO(item)));
 		return list;
 	}
 }
